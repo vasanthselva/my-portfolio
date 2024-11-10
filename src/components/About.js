d824c97b05e479f4ae5e0d1b17ec05bd7d29e95b
vasanth <123473435+vasanthselva@users.noreply.github.com>
2024-11-09 23:55:01 -0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import AboutImg from '../assets/about.svg';
-export default function About() {
+const About = () => {
     const config = {
         line1:'I am a computer science Engineer, who enjoys learning and scraping new stuff. I started coding when I was in college second year doing very basic stuff, like building static website with html.',
         line2:'I am proficient in Front end like Reactjs, Html-5, css-3, Javascript, Typescript Tailwindcss, bootstrap and Figma.',
@@ -16,10 +16,25 @@ export default function About() {
                     <p className='pb-5 font-home-font'>{config.line1}</p>
                     <p className='pb-5 font-home-font'>{config.line2}</p>
                     <p className='font-home-font'>{config.line3}</p>
+    
+            <h2 className='text-4xl  font-bold' style={styles.heading}>Contact Details</h2>
+                     <p>s/o: kr.selvakumar</p>
+                     <p>(vengatramapuram stop)</p>
+                     <p>(karaikudi) (Tamilnadu), (630313)</p>
+                     <p>(+91) 7094988549</p>
+                     <p>cssvasanth@gmail.com</p>
             </div>
             
             </div>
         </section>
     );
+};
+    const styles = {
+        heading: {
+          fontWeight: 'bold',
+          fontSize: '1.5em',
+          marginBottom: '15px',
+        }
+      };
 
-}
+export default About;
